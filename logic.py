fafa96115d216e1871d50927d70cb629ea610f35
Martin Ronning <ronningmartin@outlook.com>
2024-03-19 12:03:25 +0100

    add property categories (colors)

--- logic.py
+++ logic.py
@@ -7,7 +7,17 @@ def init(graph_str):
     graph = create_graph_from_str(graph_str)
 
     fm(graph)
+
+    draw_graph(graph)
+
     return Graph()
+
+def draw_graph(graph):
+    graph_draw(
+            graph,
+            vertex_text=graph.vertex_index,
+            vertex_fill_color = graph.vertex_properties["color"],
+            output="two-nodes-color.pdf")
 
 # this is 0-based indexing, thus (textfile_index - 1)
 def create_graph_from_str(graph_str):
@@ -26,17 +36,35 @@ def create_graph_from_str(graph_str):
 def fm(graph):
     print("fm")
     # partition graph
+    a, b = partition(graph)
+    
+    # compute gains
+    # gain = v.neighbors in B (A) - v.neighbors in A (B)
+
+
+def partition(graph):
     a = []
     b = []
+    
+    # assign property categories
+    graph.vertex_properties["color"] = graph.new_vp("string")
+    vcolor = graph.vertex_properties["color"]
+
     shuffled_vertices = list(graph.vertices())
     random.shuffle(shuffled_vertices)
+
+    # distribute vertices evenly and assign property per vertex
     for i in range(0, len(shuffled_vertices)):
+        curr_v = shuffled_vertices[i]
         if i % 2 == 0:
-            a.append(shuffled_vertices[i])
+            a.append(curr_v)
+            vcolor[curr_v] = "#1c71d8"
         else:
-            b.append(shuffled_vertices[i])
+            b.append(curr_v)
+            vcolor[curr_v] = "#2ec27e"
         i += 1
 
+    return (a, b)
 
 def multistart_ls():
     print("mls")
