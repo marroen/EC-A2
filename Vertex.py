54013f44eea46beb9955554feeeafe5c7bb67e0d
YoavVaizman <yh.vaizman@gmail.com>
2024-03-19 22:41:15 +0100

    All the setup is done. Now its time to start coding multistart, iterated and genetic ls

---
+++ Vertex.py
@@ -0,0 +1,67 @@
+class Vertex:
+    def __init__(self, number, partition, neighbors):
+        self._vertex_number = number
+        self._partitioning = partition
+        self._gain = None
+        self._connected_vertexes = neighbors
+        self._predecessor = None
+        self._successor = None
+
+    @property
+    def vertex_number(self):
+        return self._vertex_number
+
+    @vertex_number.setter
+    def vertex_number(self, value):
+        self._vertex_number = value
+
+    @property
+    def partitioning(self):
+        return self._partitioning
+
+    @partitioning.setter
+    def partitioning(self, value):
+        self._partitioning = value
+
+    @property
+    def gain(self):
+        return self._gain
+
+    @gain.setter
+    def gain(self, value):
+        self._gain = value
+
+    @property
+    def connected_vertexes(self):
+        return self._connected_vertexes
+
+    @connected_vertexes.setter
+    def connected_vertexes(self, value):
+        self._connected_vertexes = value
+
+    @property
+    def predecessor(self):
+        return self._predecessor
+
+    @predecessor.setter
+    def predecessor(self, value):
+        self._predecessor = value
+
+    @property
+    def successor(self):
+        return self._successor
+
+    @successor.setter
+    def successor(self, value):
+        self._successor = value
+
+    def __str__(self):
+        return f"Vertex Number: {self.vertex_number} " \
+               f"Partitioning: {self.partitioning} " \
+               f"Gain: {self.gain} " \
+               f"Connected Vertexes: {self.connected_vertexes} " \
+               f"Predecessor: {self.predecessor} " \
+               f"Successor: {self.successor} \n"
+
+    def __repr__(self):
+        return self.__str__()
--- logic.py
+++ logic.py
@@ -1,23 +1,31 @@
 from chromosome import Chromosome
 from graph_tool.all import *
 import random
+from Vertex import Vertex
+
 
 def init(graph_str):
     print("init")
     graph = create_graph_from_str(graph_str)
 
-    fm(graph)
+    main_list, list_0, list_1 = fm(graph)
 
-    #draw_graph(graph)
+    multistart_ls(main_list, list_0, list_1)
+    iterated_ls(main_list, list_0, list_1)
+    genetic_ls(main_list, list_0, list_1)
+
+    draw_graph(graph)
 
     return Graph()
 
+
 def draw_graph(graph):
     graph_draw(
-            graph,
-            vertex_text=graph.vertex_index,
-            vertex_fill_color = graph.vertex_properties["color"],
-            output="two-nodes-color.pdf")
+        graph,
+        vertex_text=graph.vertex_index,
+        vertex_fill_color=graph.vertex_properties["color"],
+        output="two-nodes-color.pdf")
+
 
 # this is 0-based indexing, thus (textfile_index - 1)
 def create_graph_from_str(graph_str):
@@ -28,26 +36,30 @@ def create_graph_from_str(graph_str):
         vertex_str = splitted_lines[i].split()
         n = int(vertex_str[2])
         for j in range(0, n):
-            if graph.edge(i, int(vertex_str[3+j])-1) is None:
-                graph.add_edge(i, int(vertex_str[3+j])-1)
-    
+            if graph.edge(i, int(vertex_str[3 + j]) - 1) is None:
+                graph.add_edge(i, int(vertex_str[3 + j]) - 1)
+
     return graph
+
 
 def fm(graph):
     print("fm")
     # partition graph (not the arrays)
     a, b = partition(graph)
+
     print(len(a))
     print(len(b))
-    
+
+    return setup_main_list(a, b, graph)
+
     # compute gains
     # gain = v.neighbors in B (A) - v.neighbors in A (B)
-    
+
 
 def partition(graph):
     a = set()
     b = set()
-    
+
     # assign property categories
     graph.vertex_properties["color"] = graph.new_vp("string")
     vcolor = graph.vertex_properties["color"]
@@ -68,11 +80,126 @@ def partition(graph):
 
     return (a, b)
 
-def multistart_ls():
+
+def multistart_ls(main_list, list_0, list_1):
     print("mls")
 
-def iterated_ls():
+
+def iterated_ls(main_list, list_0, list_1):
     print("ils")
 
-def genetic_ls():
+
+def genetic_ls(main_list, list_0, list_1):
     print("gls")
+
+
+def setup_main_list(a, b, graph):
+    main_list = []
+    for i in range(0, 500):
+        if graph.vertex_properties["color"][graph.vertex(i)] == "#1c71d8":  # ToDo: works?
+            main_list.append(Vertex(i, 0, [int(n) for n in graph.vertex(i).all_neighbors()]))
+        if graph.vertex_properties["color"][graph.vertex(i)] == "#2ec27e":  # ToDo: works?
+            main_list.append(Vertex(i, 1, [int(n) for n in graph.vertex(i).all_neighbors()]))
+
+    list_0 = {i: -1 for i in range(-30, 31)}
+    list_1 = {i: -1 for i in range(-30, 31)}
+
+    numbers = list(range(0, 500))
+    random.shuffle(numbers)
+
+    for j in range(0, 500):
+        i = numbers[j]
+        gain = 0
+        if main_list[i].partitioning == 0:
+            if len(main_list[i].connected_vertexes) > 0:
+                for n in main_list[i].connected_vertexes:
+                    if main_list[n].partitioning == 0:
+                        gain -= 1
+                    if main_list[n].partitioning == 1:
+                        gain += 1
+            if list_0[gain] == -1:
+                list_0[gain] = i
+                main_list[i].gain = gain
+            else:
+                previous_number = list_0[gain]
+                main_list[previous_number].successor = i
+                main_list[i].predecessor = previous_number
+                main_list[i].gain = gain
+                list_0[gain] = i
+
+        else:
+            if len(main_list[i].connected_vertexes) > 0:
+                for n in main_list[i].connected_vertexes:
+                    if main_list[n].partitioning == 1:
+                        gain -= 1
+                    if main_list[n].partitioning == 0:
+                        gain += 1
+            if list_1[gain] == -1:
+                list_1[gain] = i
+                main_list[i].gain = gain
+            else:
+                previous_number = list_1[gain]
+                main_list[previous_number].successor = i
+                main_list[i].predecessor = previous_number
+                main_list[i].gain = gain
+                list_1[gain] = i
+
+    # print(main_list)
+    # print(list_0)
+    # print(list_1)
+
+    return main_list, list_0, list_1
+
+
+# Ignore, this is old stuff of yoav, but I want to keep it in for now
+
+"""
+    # List with a tuple
+    # 0 = # of vertex
+    # 1 = partitioning (0 or 1)
+    # 2 = gain
+    # 3 = connected vertex
+    # 4 = predecessor
+    # 5 = successor
+    
+    main_list = []
+    for i in range(0, 500):
+        if i in a:
+            tup = [i, 0, None, [int(n) for n in graph.vertex(i).all_neighbors()], None, None]
+            main_list.append(tup)
+        if i in b:
+            tup = [i, 1, None, [int(n) for n in graph.vertex(i).all_neighbors()], None, None]
+            main_list.append(tup)
+            
+    for j in reversed(range(0, 500)):
+        i = numbers[j]
+        gain = 0
+        if main_list[i][1] == 0:
+            for n in main_list[i][3]:
+                if main_list[n][1] == 0:
+                    gain -= 1
+                if main_list[n][1] == 1:
+                    gain += 1
+            if list_0[gain] == -1:
+                list_0[gain] = i
+                main_list[i][2] = gain
+            else:
+                previous_number = list_0[gain]
+                main_list[previous_number][5] = i
+                main_list[i][4] = previous_number
+                main_list[i][2] = gain
+        else:
+            for n in main_list[i][3]:
+                if main_list[n][1] == 1:
+                    gain -= 1
+                if main_list[n][1] == 0:
+                    gain += 1
+            if list_1[gain] == -1:
+                list_1[gain] = i
+                main_list[i][2] = gain
+            else:
+                previous_number = list_1[gain]
+                main_list[previous_number][5] = i
+                main_list[i][4] = previous_number
+                main_list[i][2] = gain
+"""
